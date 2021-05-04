4b58bd0ced1cae4c7f4829c4c9fd4808aaebaa8d
Qingyang (Tom) Xi <tom.xi@nyu.edu>
2021-05-04 11:32:23 -0400

    fixed type error

--- prockey.py
+++ prockey.py
@@ -9,7 +9,7 @@ from tqdm import tqdm
 aug_path = '/scratch/qx244/data/gskey/augmentation'
 # key_machine = madmom.features.key.CNNKeyRecognitionProcessor()
 
-worker = sys.argv[1]
+worker = int(sys.argv[1])
 file_list = glob.glob(os.path.join(aug_path, '*.{:02d}.ogg'.format(worker)))
 
 print(len(file_list))
