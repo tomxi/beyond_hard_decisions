403c9ffe826a0e713e82f0ab9cca9a92622e4c43
Qingyang (Tom) Xi <tom.xi@nyu.edu>
2021-05-04 11:30:39 -0400

    1st multi worker script test

--- prockey.py
+++ prockey.py
@@ -1,3 +1,15 @@
 import madmom
 import numpy as np
-import jams
+import glob
+import os
+import sys
+
+from tqdm import tqdm
+
+aug_path = '/scratch/qx244/data/gskey/augmentation'
+# key_machine = madmom.features.key.CNNKeyRecognitionProcessor()
+
+worker = sys.argv[1]
+file_list = glob.glob(os.path.join(aug_path, '*.{:02d}.ogg'.format(worker)))
+
+print(len(file_list))
